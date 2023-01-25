f167269238cf1ba1acc1fba0684857b032d2dff8
Lorisse Fray <mohammed-el-amine.abdelmoudjib@etu.univ-amu.fr>
2023-01-25 12:28:48 +0100

    ok

--- js/script.js
+++ js/script.js
@@ -5,9 +5,12 @@ function poundsToKg(poundsWeight) {
 const specificationsWeight = document.querySelector('#specifications-weight');
 
 window.onload = function () {
+
+  
   const poundsWeight = parseInt(specificationsWeight.textContent);
   console.log({ poundsWeight });
   const kgWeight = Math.round(poundsToKg(poundsWeight) * 100) / 100;
   console.log({ kgWeight });
   specificationsWeight.textContent = `${kgWeight}kg`;
 }
+
